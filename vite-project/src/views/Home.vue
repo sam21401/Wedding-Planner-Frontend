6275a498675d31089b23518076ad05102f0d0ccc
Paweł <pawel.zebrowski@studenci.collegiumwitelona.pl>
2025-01-13 08:43:40 +0100

    s

--- vite-project/src/views/Home.vue
+++ vite-project/src/views/Home.vue
@@ -1,9 +1,5 @@
 <template>
-  <div class="home">
-    <h1>Witaj na stronie głównej!</h1>
-    <p>Tu możesz wyświetlić zawartość strony głównej.</p>
-    <!-- Inne specyficzne dla strony głównej elementy -->
-  </div>
+  
 </template>
 
 <script setup lang="ts">
